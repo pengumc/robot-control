c829a2c73858c69f87ce6aa883e4b3189f2633e2
Pengu MC <michiel.van.der.coelen@gmail.com>
2011-06-07 23:20:15 +0200

    cleanup

--- src/main.cpp
+++ src/main.cpp
@@ -2,19 +2,9 @@
 //linux g++ test.cpp -o test -lpthread
 //------------------------------------------------------------------------------
 #define __MAIN__
-#include <iostream>
 #include <stdint.h>
 #include <math.h>
 #define PI M_PI
-#include <stdio.h>
-//thread stuff is included for backup reasons
-#ifdef _WIN32
-    #include <conio.h>
-    #include <win32_input.cpp>
-		#define usleep(x) Sleep(x/1000)
-#else
-    #include <pthreads_input.cpp>
-#endif
 
 #include "CSolver.cpp"
 #include "CServo.cpp"
